9178391af45ac968bdad0f89851271e5a88fefdc
Amir860k <amyrrdakr016@gmail.com>
2025-12-03 15:01:55 +0330

    Create conn.php

---
+++ conn.php
@@ -0,0 +1,17 @@
+
+<?php
+function connect($servername,$username,$password,$database)
+{
+$conn=mysqli_connect($servername,$username, $password, $database);
+  if ($conn)
+  {
+    echo"درست";
+    return ($conn);
+  }
+  else
+  {
+    echo"نادرست";
+  }
+     mysqli_connect($servername , $username , $password , $database);
+}
+?>
